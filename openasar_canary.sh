14acf7dd36abe91506480e6a5d3c20c254fd19a5
Dre Dall'Ara <42755661+singlekeycap@users.noreply.github.com>
2022-11-15 16:22:10 -0500

    Add canary (stable will be fixed later)

---
+++ openasar_canary.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+killall -9 Discord\ Canary
+sleep 5
+
+mv /Applications/Discord\ Canary.app/Contents/Resources/app.asar /Applications/Discord\ Canary.app/Contents/Resources/app.asar.bak
+curl --silent -k -o app.asar -L "https://github.com/GooseMod/OpenAsar/releases/download/nightly/app.asar"
+
+if [ -s app.asar ];
+then
+	mv app.asar /Applications/Discord\ Canary.app/Contents/Resources/app.asar
+	rm /Applications/Discord\ Canary.app/Contents/Resources/app.asar.bak
+	echo "Done! Open Discord to apply changes."
+else
+	mv /Applications/Discord\ Canary.app/Contents/Resources/app.asar.bak /Applications/Discord\ 
+Canary.app/Contents/Resources/app.asar
+	echo "An error occurred! Installation cannot proceed. Is this script outdated?"
+fi
