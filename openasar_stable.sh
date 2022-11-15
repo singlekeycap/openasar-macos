29225d3e2ef761599a195329b73d97ee38fe0942
Dre Dall'Ara <42755661+singlekeycap@users.noreply.github.com>
2022-11-15 16:40:29 -0500

    Update openasar_stable.sh

--- openasar_stable.sh
+++ openasar_stable.sh
@@ -3,7 +3,7 @@
 killall -9 Discord
 sleep 5
 
-cp /Applications/Discord.app/Contents/Resources/app.asar /Applications/Discord.app/Contents/Resources/app.asar.bak
+mv /Applications/Discord.app/Contents/Resources/app.asar /Applications/Discord.app/Contents/Resources/app.asar.bak
 curl --silent -k -o app.asar -L "https://github.com/GooseMod/OpenAsar/releases/download/nightly/app.asar"
 
 if [ -s app.asar ];
