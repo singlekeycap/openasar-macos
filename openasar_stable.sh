b8d699c477fc92d02d5bedb2dc457f55cfc5e2ae
Dre Dall'Ara <42755661+singlekeycap@users.noreply.github.com>
2022-11-15 16:40:08 -0500

    Fixed stable error checking

--- openasar_stable.sh
+++ openasar_stable.sh
@@ -8,8 +8,10 @@ curl --silent -k -o app.asar -L "https://github.com/GooseMod/OpenAsar/releases/d
 
 if [ -s app.asar ];
 then
-	mv app.asar /Applications/Discord.app/Contents/Resources/app.asar
-	echo "Done! Open Discord to apply changes."
+        mv app.asar /Applications/Discord.app/Contents/Resources/app.asar
+        rm /Applications/Discord.app/Contents/Resources/app.asar.bak
+        echo "Done! Open Discord to apply changes."
 else
-	echo "An error occurred! Installation cannot proceed. Is this script outdated?"
+        mv /Applications/Discord.app/Contents/Resources/app.asar.bak /Applications/Discord.app/Contents/Resources/app.asar
+        echo "An error occurred! Installation cannot proceed. Is this script outdated?"
 fi
